2dcac3bf5842f718f94d17a43634d11a167b0a4d
ashneeldas2 <adas2@stuy.edu>
2018-03-12 12:06:05 -0400

    dun

---
+++ main.py
@@ -0,0 +1,25 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+transform = new_matrix()
+
+# print_matrix( make_translate(3, 4, 5) )
+# print
+# print_matrix( make_scale(3, 4, 5) )
+# print
+# print_matrix( make_rotX(math.pi/4) )
+# print
+# print_matrix( make_rotY(math.pi/4) )
+# print
+# print_matrix( make_rotZ(math.pi/4) )
+
+#add_circle(edges, 250, 250, 0, 200, 0.001)
+#draw_lines(edges, screen, color)
+#display(screen)
+parse_file( 'script2', edges, transform, screen, color )
